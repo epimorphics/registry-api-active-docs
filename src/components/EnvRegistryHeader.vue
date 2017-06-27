2491b10074eb2a5ea6e69c62d2ab13f1cae50a72
Ian Dickinson <i.j.dickinson@gmail.com>
2017-06-27 23:34:29 +0100

    Progress on issue #2 - completed adding GDS-style top nav bar, with Defra department colour line

--- src/components/EnvRegistryHeader.vue
+++ src/components/EnvRegistryHeader.vue
@@ -1,19 +1,31 @@
 <template>
-  <nav class="c-env-registry-header">
-    <h1>Environment Registry</h1>
-    <el-menu theme="dark" :default-active="activeIndex"mode="horizontal">
-      <el-menu-item index="1"><a href=''>Browse</a></el-menu-item>
-      <el-menu-item index="2"><a href=''>About</a></el-menu-item>
-      <el-submenu index="3">
-        <template slot="title">Advanced</template>
-        <el-menu-item index="3-1"><a href='/registry/ui/login?return=https://environment-registry.epimorphics.net/registry/'>Login</a></el-menu-item>
-        <el-menu-item index="3-2"><a href='/registry/ui/api-documentation'>API documentation</a></el-menu-item>
-        <el-menu-item index="3-3"><a href='/registry/?showStatus=accepted'>Show all entries</a></el-menu-item>
-        <el-menu-item index="3-4"><a href='/registry/ui/actions/check-page'>Check URI</a></el-menu-item>
-        <el-menu-item index="3-5"><a href='/registry/ui/sparql-form'>SPARQL</a></el-menu-item>
-      </el-submenu>
-    </el-menu>
-  </nav>
+  <div>
+    <nav class="c-env-registry-header">
+      <el-row :gutter='0'>
+        <el-col :lg='{span: 20, offset: 2}' :md='{span: 24, offset: 0}'>
+          <h1 class='c-env-registry-header--menu-item'>Environment Registry</h1>
+
+          <el-menu theme="dark" :default-active="activeIndex"mode="horizontal" class='c-env-registry-header--menu-item'>
+            <el-menu-item index="1"><a href=''>Browse</a></el-menu-item>
+            <el-menu-item index="2"><a href=''>About</a></el-menu-item>
+            <el-submenu index="3">
+              <template slot="title">Advanced</template>
+              <el-menu-item index="3-1"><a href='/registry/ui/login?return=https://environment-registry.epimorphics.net/registry/'>Login</a></el-menu-item>
+              <el-menu-item index="3-2"><a href='/registry/ui/api-documentation'>API documentation</a></el-menu-item>
+              <el-menu-item index="3-3"><a href='/registry/?showStatus=accepted'>Show all entries</a></el-menu-item>
+              <el-menu-item index="3-4"><a href='/registry/ui/actions/check-page'>Check URI</a></el-menu-item>
+              <el-menu-item index="3-5"><a href='/registry/ui/sparql-form'>SPARQL</a></el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </nav>
+    <el-row>
+      <el-col :lg='{span: 20, offset: 2}' :md='{span: 24, offset: 0}'>
+        <div class='u-department-colour'></div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -27,10 +39,10 @@
     color: white;
     background-color: black;
 
-    > * {
+    &--menu-item {
       display: inline-block;
       vertical-align: middle;
-      margin-left: 2em;
+      margin-right: 2em;
     }
 
     ul.el-menu--horizontal.el-menu--dark {
