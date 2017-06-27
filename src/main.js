18a589fd3788ec3f0ced3a89df1fd0aa0d7d5381
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-06-27 14:18:14 +0100

    Enable use of custom theme in ElementUI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+/* eslint-disable import/first */
+import './styles/theme/index.css';
+import 'element-ui/lib/theme-default/index.css';
+
 import Vue from 'vue';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';
 import locale from 'element-ui/lib/locale/lang/en';
 
 import 'vue-awesome/icons';
