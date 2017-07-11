92ae34451b578b7371faa3ea721586faf0cdd2ff
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-07-11 15:58:18 +0100

    Use v-html on embedded span element to show HTML in control labels. Fixes #34

--- src/components/parameter/ApiParamDate.vue
+++ src/components/parameter/ApiParamDate.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='c-api-param c-api-param--date'>
-    <el-checkbox v-model='checked' v-on:change='enableOption'>{{ apiParam.displayName() }}</el-checkbox>
+    <el-checkbox v-model='checked' v-on:change='enableOption'><span v-html='apiParam.displayName()' /></el-checkbox>
     <el-date-picker v-model='value' type='date' size='small' v-bind:disabled='!checked' v-on:change='updateOption'></el-date-picker>
   </div>
 </template>
