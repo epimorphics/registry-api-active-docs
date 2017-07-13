8830fda2c7153fbec11c9363a97a4e31e696a682
Ian Dickinson <ian.dickinson@epimorphics.com>
2017-07-13 12:08:20 +0100

    Reduce log noise during unit test by catching and logging rejected promise

--- src/services/simple-auth.js
+++ src/services/simple-auth.js
@@ -10,9 +10,15 @@ let creds = {};
 
 Axios
   .get(AUTH_FILE)
-  .then((file) => {
-    creds = file.data;
-  });
+  .then(
+    (file) => {
+      creds = file.data;
+    },
+    (reason) => {
+      // eslint-disable-next-line
+      console.log(`Failed to load auth.json, because '${reason}'.`);
+    },
+);
 
 export default {
   credentials: () => creds,
